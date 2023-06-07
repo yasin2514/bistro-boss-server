8370abfcdde7f2f849a76743330d7db80702ab6b
yasin2514 <yasinkhanrabbi1999@gmail.com>
2023-06-08 01:21:31 +0600

    bug fixed

--- index.js
+++ index.js
@@ -143,7 +143,7 @@ async function run() {
         app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
-            const result = await menuCollection().deleteOne(query);
+            const result = await menuCollection.deleteOne(query);
             res.send(result);
 
         })
